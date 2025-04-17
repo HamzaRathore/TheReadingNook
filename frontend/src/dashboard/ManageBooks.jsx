ba85cb4471629cf7f832cc99c05a42cfb3edf976
HamzaRathore <128692254+HamzaRathore@users.noreply.github.com>
2025-04-17 20:39:15 +0500

    ManageBooks.jsx

--- frontend/src/dashboard/ManageBooks.jsx
+++ frontend/src/dashboard/ManageBooks.jsx
@@ -141,7 +141,10 @@ const ManageBooks = () => {
                             )}
                           </td>
                           <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                           <Link to={`https://the-reading-nook.vercel.app/admin/dashboard/edit-books/${book._id}`}>
+                          <Link 
+                             to={`/admin/dashboard/edit-books/${book._id}`}
+                             onClick={() => console.log('Navigating to edit page for book:', book._id)}
+                                >
                               <button className="text-indigo-600 cursor-pointer hover:text-indigo-900 mr-4">
                                 <FaEdit className="inline mr-1" /> Edit
                               </button>
